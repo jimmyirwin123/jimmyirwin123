24817896680267e57852f8208bfdf3370ce150d4
jimmyirwin123 <49951033+jimmyirwin123@users.noreply.github.com>
2021-01-28 17:15:18 +0000

    Add files via upload

---
+++ sound.js
@@ -0,0 +1,32 @@
+
+
+function textSwitch() {
+
+    var btn = document.getElementById("myButton");
+    if (btn.value == "Play") {
+        btn.value = "Pause";
+        btn.innerHTML = "Play";
+    } else {
+        btn.value = "Play";
+        btn.innerHTML = "Pause";
+        }
+    };
+    
+    
+// AUDIO TOGGLE PLAY/PAUSE ***********************
+
+var myAudio = document.getElementById("myAudio");
+var isPlaying = false;
+
+
+function togglePlay() {
+    isPlaying ? myAudio.pause() : myAudio.play();
+};
+
+myAudio.onplaying = function() {
+    isPlaying = true;
+};
+
+myAudio.onpause = function() {
+    isPlaying = false;
+};
