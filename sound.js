4bb7f5495621d8ab77bafabdb90f4fbaae3c391a
jimmyirwin123 <49951033+jimmyirwin123@users.noreply.github.com>
2021-01-30 13:39:58 +0000

    Update sound.js

--- sound.js
+++ sound.js
@@ -38,7 +38,6 @@ myAudio.onpause = function() {
 document.addEventListener('keyup', event => {
     if (event.code === 'Space') {
         togglePlay();
-        textSwitch();
         console.log('Space pressed');
     }
 
